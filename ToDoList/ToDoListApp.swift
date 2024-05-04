36f3a3fa8be503267e6a6c5ab0287e1797755041
Martí Espinosa <martiespinosa99@gmail.com>
2024-05-04 16:00:53 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Martí Espinosa Farran on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
